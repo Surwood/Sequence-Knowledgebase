834defedbc4c42126c70961e39151e76f88df2d7
jay <jaylong255@gmail.com>
2017-04-11 14:14:48 -0500

    auto save articles

--- wp-content/plugins/sequence-knowledgebase/cron/report_emails.php
+++ wp-content/plugins/sequence-knowledgebase/cron/report_emails.php
@@ -1,10 +1,11 @@
 <?php
 
 
-  // $parse_uri = explode( '/wp-content/', $_SERVER['SCRIPT_FILENAME'] );
-  // require_once( $parse_uri[0] . '/wp-load.php' );
-  require_once( '/home/seqgxp2100/public_html/dev.sequencegxp.com/wp-load.php' );
+  $parse_uri = explode( '/wp-content/', $_SERVER['SCRIPT_FILENAME'] );
+  require_once( $parse_uri[0] . '/wp-load.php' );
+  // require_once( '/home/seqgxp2100/public_html/dev.sequencegxp.com/wp-load.php' );
 
+// echo 'test';
 
 
   $args = array(
@@ -19,7 +20,7 @@
   $high_rating = 0;
   $most_views = 0;
 
-  
+
 
   foreach($posts as $post){
     $avg_rating = 0;
@@ -61,7 +62,7 @@
   $header .= "MIME-Version: 1.0\r\n";
   $header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-  $message = "Your article '". $post->post_title ."' received and average\r\n rating of ". $high_rating ." and got ". $most_views ." page views. "
+  $message = "Your article '". $post->post_title ."' received and average\r\n rating of ". $high_rating ." and got ". $most_views ." page views. ";
 
   $body = '
   <html>
@@ -157,6 +158,127 @@
     $header
   );
 
-  echo "Mail sent";
+  echo "Mail sent to highest ranking author, " . $author->user_fullname . "<hr />";
+  echo $body . "<hr />";
+
+  // send email to expired pending posts
+  $args = array(
+    'post_type'=>'skb_article',
+    'post_status' =>  'pending'
+  );
+  $posts = get_posts($args);
+  foreach ($posts as $post){
+
+    $days = (time() - strtotime($post->post_modified)) / ( 60 * 60 * 24 );
+    // $days = $post->post_modified;
+    // echo $days;
+    // die('test');
+    // echo "<h1>". $days ."</h1>";
+
+    if($days > 7){
+      $header = "From: gsequence@sequenceqcs.com\r\n";
+      $header .= "Reply-To: no-reply\r\n";
+      $header .= "MIME-Version: 1.0\r\n";
+      $header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+      $message = "Your article '". $post->post_title ."' has still not been approved. \r\n It has been ". floor($days) ." days since you last submitted changes to this article. ";
+      $body = send_mail_template($post,$message);
+      echo $body;
+        // die($post->modification_date . "test");
+      mail(
+        $author->user_email,
+        'Article still not approved. ',
+        $body,
+        $header
+      );
+    }
+  }
+
+  function send_mail_template($post,$message){
+    $body = '
+    <html>
+      <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+         <title>Nettuts Email Newsletter</title>
+         <style type="text/css">
+            a {color: #4A72AF;}
+            body, #header h1, #header h2, p {margin: 0; padding: 0;}
+            #main {border: 1px solid #cfcece;}
+            img {display: block;}
+            #top-message p, #bottom-message p {color: #3f4042; font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
+            #header h1 {color: #ffffff !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 24px; margin-bottom: 0!important; padding-bottom: 0; }
+            #header h2 {color: #ffffff !important; font-family: Arial, Helvetica, sans-serif; font-size: 24px; margin-bottom: 0 !important; padding-bottom: 0; }
+            #header p {color: #ffffff !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 12px;  }
+            h1, h2, h3, h4, h5, h6 {margin: 0 0 0.8em 0;}
+            h3 {font-size: 28px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
+            h4 {font-size: 22px; color: #4A72AF !important; font-family: Arial, Helvetica, sans-serif; }
+            h5 {font-size: 18px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
+            p {font-size: 12px; color: #444444 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; line-height: 1.5;}
+         </style>
+      </head>
+      <body>
+        <table width="100%" cellpadding="0" cellspacing="0" bgcolor="e4e4e4"><tr><td>
+        <table id="top-message" cellpadding="20" cellspacing="0" width="600" align="center">
+            <tr>
+              <td align="center">
+                <p>Trouble viewing this email? <a href="#">View in Browser</a></p>
+              </td>
+            </tr>
+          </table><!-- top message -->
+        <table id="main" width="600" align="center" cellpadding="0" cellspacing="15" bgcolor="ffffff">
+            <tr>
+              <td>
+                <table id="header" cellpadding="10" cellspacing="0" align="center" bgcolor="8fb3e9">
+                  <tr>
+                    <td width="100%" bgcolor="#ffffff"><a href="'. site_url() .'/dashboard/"><img alt="SEQUENCE KNOWLEDGEBASE" src="'. SKB_PLUGIN_URL .'images/logo.png" /></td>
+                  </tr>
+                </table><!-- header -->
+              </td>
+            </tr><!-- header -->
+            <tr>
+              <td>
+                <table id="content-2" cellpadding="0" cellspacing="0" align="center">
+                <tr>
+                  <td align="center" width="100%">
+                    <p>Click <a href="'. site_url() .'/dashboard/">here</a> to visit Sequence Knowledgebase Dashboard. </p>
+                  </td>
+                </tr>
+                </table><!-- content-2 -->
+              </td>
+            </tr><!-- content-2 -->
+            <tr>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <table id="content-1" cellpadding="0" cellspacing="0" align="center">
+                  <tr>
+                    <td width="15"></td>
+                    <td width="375" valign="top" colspan="3">
+                      <h3>'. $post->post_title .'</h3>
+                      <h4>'. $message .'</h4>
+                    </td>
+
+                  </tr>
+                </table><!-- content 1 -->
+              </td>
+            </tr><!-- content 1 -->
+            <tr>
+              <td>
+                <table id="content-2" cellpadding="0" cellspacing="0" align="center">
+                <tr>
+                  <td align="center" width="100%">
+                    <p>Click <a href="'. site_url() .'dashboard/view-article/?article='. $post->ID .'">here</a> to view article. </p>
+                  </td>
+                </tr>
+                </table><!-- content-2 -->
+              </td>
+            </tr><!-- content-2 -->
+          </table><!-- main -->
+        </td></tr></table><!-- wrapper -->
+      </body>
+    </html>
+    ';
+    return $body;
+  }
 
  ?>
