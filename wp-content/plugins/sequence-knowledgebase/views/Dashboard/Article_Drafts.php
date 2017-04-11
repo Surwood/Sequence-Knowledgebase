86c280e19aeda892b8da7bf321b4cabcd3f1a955
jay <jaylong255@gmail.com>
2017-04-11 12:11:44 -0500

    auto save articles

--- wp-content/plugins/sequence-knowledgebase/views/Dashboard/Article_Drafts.php
+++ wp-content/plugins/sequence-knowledgebase/views/Dashboard/Article_Drafts.php
@@ -1,15 +1,17 @@
-<div id="sequence-article-drafts">test
-<?php
-if(count($article_drafts)>0){
-  ?><hr /><h3>Your Article Drafts</h3><hr /><?php
-  foreach($article_drafts as $post){
-    ?>
-      <a href="<?php echo site_url() . "/dashboard/add-article/?article=" . $post->ID; ?>">
-        <h3><?php echo $post->post_title; ?></h3>
-      </a>
-    <?php
-  }
-}
- ?>
+<div id="sequence-article-drafts">
+  <?php if(count($article_drafts)>0){ ?>
+      <div href="#article-draft-results" data-toggle="collapse" class="article-widget-header">
+        <hr />
+        <h3>Your Article Drafts</h3>
+        <hr />
+      </div>
+      <div id="article-draft-results" class="user-article-results collapse">
+        <?php foreach($article_drafts as $post){ ?>
+            <a href="<?php echo site_url() . "/dashboard/add-article/?article=" . $post->ID; ?>">
+              <h3><?php echo $post->post_title; ?></h3>
+            </a>
+        <?php } ?>
+      </div>
+  <?php } ?>
   <hr />
 </div>
