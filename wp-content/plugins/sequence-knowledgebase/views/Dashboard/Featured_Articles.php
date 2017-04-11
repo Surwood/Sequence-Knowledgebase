424606b50fed0a41f803259861d7e07d819f49f8
jay <jaylong255@gmail.com>
2017-04-11 12:16:21 -0500

    auto save articles

--- wp-content/plugins/sequence-knowledgebase/views/Dashboard/Featured_Articles.php
+++ wp-content/plugins/sequence-knowledgebase/views/Dashboard/Featured_Articles.php
@@ -1,14 +1,27 @@
 <div id="sequence-featured-articles">
-
   <hr />
-
     <h3>Featured Articles</h3>
-
   <hr />
-
     <div class="article-results"></div>
-
-
   <hr />
-
 </div>
+
+
+<script>
+  jQuery(document).ready(function(){
+    $.post(
+      '<?php echo SKB_PLUGIN_URL; ?>controllers/ajax_controller.php?method=get_search_results',
+      {
+        look: "",
+        postCategories1: -1,
+        postCategories2: -1,
+        postCategories3: -1,
+        postCategories4: -1,
+        posts_per_page: 5
+      },
+      function(data){
+        $('#sequence-featured-articles .article-results').html(data);
+      }
+    );
+  });
+</script>
--- wp-content/plugins/sequence-knowledgebase/views/Dashboard/User_Articles.php
+++ wp-content/plugins/sequence-knowledgebase/views/Dashboard/User_Articles.php
@@ -17,35 +17,17 @@
         postCategories2: -1,
         postCategories3: -1,
         postCategories4: -1,
-        posts_per_page: 5
+        posts_per_page: 5,
+        user: 1
       },
       function(data){
-        $('#sequence-featured-articles .article-results').html(data);
-        $.post(
-          '<?php echo SKB_PLUGIN_URL; ?>controllers/ajax_controller.php?method=get_search_results',
-          {
-            look: "",
-            postCategories1: -1,
-            postCategories2: -1,
-            postCategories3: -1,
-            postCategories4: -1,
-            posts_per_page: 5,
-            user: 1
-          },
-          function(data){
-            $('#sequence-user-articles .user-article-results').html(data);
-          }
-        );
+        $('#sequence-user-articles .user-article-results').html(data);
+        jQuery('.ratingWrapper').each(function(event){
+          var id = '#'+jQuery(this).attr('id');
+          var rating = jQuery(this).attr('data-rating');
+          jQuery(id).rateYo({ rating: rating, starWidth: "20px", readOnly: true });
+        });
       }
     );
-
-    jQuery('.ratingWrapper').each(function(event){
-  		var id = '#'+jQuery(this).attr('id');
-  		var rating = jQuery(this).attr('data-rating');
-  		jQuery(id).rateYo({ rating: rating, starWidth: "20px", readOnly: true });
-  	});
-
   });
-
-
 </script>
